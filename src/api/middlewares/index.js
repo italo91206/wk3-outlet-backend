51ae27eade6dfb19f52c00274768d20cabb2e488
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-06-12 23:09:48 -0300

    unnecessary thing removed

--- src/api/middlewares/index.js
+++ src/api/middlewares/index.js
@@ -9,7 +9,8 @@ import cacheMiddleware from './cacheMiddleware';
 /**
  * Array de middlewares.
  */
-const middlewares = [jwtMiddleware, cacheMiddleware];
+//const middlewares = [jwtMiddleware, cacheMiddleware];
+const middlewares = [jwtMiddleware];
 const middlewaresRecupera = [jwtMiddlewareRecuperaSenha];
 const middlewaresAtivaConta = [jwtMiddlewareValidaConta];
 export { middlewares, middlewaresRecupera, middlewaresAtivaConta };
