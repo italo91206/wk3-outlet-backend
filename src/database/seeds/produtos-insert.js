8ba8a2f567f2b2d621114be114687b4c84fa8f3b
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-07-19 23:22:01 -0300

    new seed added

---
+++ src/database/seeds/produtos-insert.js
@@ -0,0 +1,48 @@
+exports.seed = function(knex){
+    return knex('produtos').insert([
+        {
+            nome: 'Camiseta Bonita',
+            preco: 149.90,
+            estoque: 10,
+            peso: 0.450,
+            url: 'camiseta-bonita',
+            custo: 49.90,
+            lucro: 100.00,
+            modelo_id: '',
+            marca_id: ''
+        },
+        {
+            nome: 'Camiseta Mais Bonita que a outra',
+            preco: 189.90,
+            estoque: 20,
+            peso: 0.650,
+            url: 'camiseta-mais-bonita-que-a-outra',
+            custo: 89.90,
+            lucro: 100.00,
+            modelo_id: '',
+            marca_id: ''
+        },
+        {
+            nome: 'Camiseta Feia',
+            preco: 89.90,
+            estoque: 10,
+            peso: 0.450,
+            url: 'camiseta-feia',
+            custo: 49.90,
+            lucro: 40.00,
+            modelo_id: '',
+            marca_id: ''
+        },
+        {
+            nome: 'Camiseta Horrorosa',
+            preco: 359.90,
+            estoque: 10,
+            peso: 0.450,
+            url: 'camiseta-feia',
+            custo: 59.90,
+            lucro: 300.00,
+            modelo_id: '',
+            marca_id: ''
+        }
+    ])
+}
