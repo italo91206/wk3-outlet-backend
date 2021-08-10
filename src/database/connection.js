ca79dc01f40860d5469898ed208e585ba63665e4
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-08-10 19:59:12 -0300

    added production-ready DB configs

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,12 @@
 import knex from 'knex';
 import configuration from '../../knexfile';
+import dotenv from 'dotenv';
 
-const connection = knex(configuration.development);
+let connection = process.env.NODE_ENV
+
+if(connection == 'development')
+    connection = knex(configuration.development);
+else
+    connection = knex(configuration.production)
 
 module.exports = connection;
