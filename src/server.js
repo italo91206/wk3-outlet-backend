3d34f7e4cccaae225cb8bfef4cb63f31941b5529
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-08-10 19:28:24 -0300

    try

--- src/server.js
+++ src/server.js
@@ -2,14 +2,17 @@ import app from './app';
 import config from './config';
 import logger from './utils/logger';
 
-async function startServer(){
-    app.listen(config.port, err =>{
-        if(err){
-            logger.error(`Erro ao iniciar a aplicação: ${err.stack}`);
-            process.exit(1);
-        }
-        logger.info(`Servidor rodando na porta: ${config.port}`)
-    })
+async function startServer() {
+  app.listen(config.port, err => {
+    if (err) {
+      logger.error(`Erro ao iniciar a aplicação: ${err.stack}`);
+      process.exit(1);
+    }
+    else {
+      logger.info(`running in ${process.env.NODE_ENV} environment`)
+      logger.info(`Servidor rodando na porta: ${config.port}`)
+    }
+  })
 }
 
 startServer();
