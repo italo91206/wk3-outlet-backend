8fee1f81ff76104974ae2ed13e84d7fd22f58920
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-08-16 22:06:24 -0300

    better error messages

--- src/api/routes/login.js
+++ src/api/routes/login.js
@@ -1,34 +1,34 @@
 import express from 'express';
 import service from '../../app/login/loginService'
-import { middlewares } from '../middlewares';
+// import { middlewares } from '../middlewares';
 
 class LoginRouter {
-    /**
-     * @constructor
-     */
-    constructor() {
-      this.router = express.Router();
-      this.middlewares();
-      this.routes();
-    }
-    
-    middlewares(){
-      this.router.use(middlewares);
-    }
-  
-    routes() {
+  /**
+   * @constructor
+   */
+  constructor() {
+    this.router = express.Router();
+    // this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.router.use(middlewares);
+  }
+
+  routes() {
     //   para fazer o login
-      this.router.post('/login', async (req, res) => {
-          let dados;
-          try{
-            dados = await service.handleLogin(req);
-            res.status(200).json(dados);
-          }
-          catch(err){
-            res.status(400).json({ success: false, error: err.message });
-          }
-        });
-    }
+    this.router.post('/login', async (req, res) => {
+      let dados;
+      try {
+        dados = await service.handleLogin(req);
+        res.status(200).json({ success: true, data: dados });
+      }
+      catch (err) {
+        res.status(200).json({ success: false, message: err.message });
+      }
+    });
+  }
 }
-  
+
 export default new LoginRouter().router;
