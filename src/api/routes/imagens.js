ba447dc1b06db4b7cb24c731f8eb4e7009958175
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-08-30 08:27:31 -0300

    formating

--- src/api/routes/imagens.js
+++ src/api/routes/imagens.js
@@ -4,12 +4,12 @@ import { middlewares } from '../middlewares';
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function(req, file, callback) {
-    callback(null, './src/public');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.originalname);
-  }
+	destination: function (req, file, callback) {
+		callback(null, './src/public');
+	},
+	filename: function (req, file, callback) {
+		callback(null, file.originalname);
+	}
 });
 
 const upload = multer({ storage: storage });
@@ -21,16 +21,16 @@ class ImagensRouter {
 		this.routes();
 	}
 
-	middlewares(){
+	middlewares() {
 		this.router.use(middlewares);
 	}
 
-	routes(){
-		this.router.post('/upload', upload.array('fileimage'), (req, res) => {
-			console.log(req.body);
-			console.log(req.files);
-			if(!req.files)
-				res.status(200).json({ success: false, message: 'Algo aconteceu com o upload de imagens.'});
+	routes() {
+		this.router.post('/upload', upload.array('fileimage', 10), (req, res) => {
+			// console.log(req.files);
+
+			if (!req.files)
+				res.status(200).json({ success: false, message: 'Algo aconteceu com o upload de imagens.' });
 			else
 				res.status(200).json({ success: true });
 		})
