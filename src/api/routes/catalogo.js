d56ed7a26bead617b002e4ed378fbd012ecf4179
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-10-08 22:05:54 -0300

    new route added

---
+++ src/api/routes/catalogo.js
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { middlewares } from '../middlewares';
+import service from './../../app/catalogo/catalogoService';
+
+class CatalogoRouter {
+  constructor() {
+    this.router = Router();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.router.use(middlewares);
+  }
+
+  routes() {
+    this.router.get('/produto', async (req, res) => {
+      let dados;
+      let { url } = req.query;
+      try {
+        dados = await service.handleGetProduto(url);
+        res.status(200).json({ success: true, data: dados });
+      }
+      catch (err) {
+        res.status(200).json({ success: false, message: err.message });
+      }
+    })
+  }
+}
+
+export default new CatalogoRouter().router;
