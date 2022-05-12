0a158c7d1624e48cf529a26b3ca22af28a91a1c7
italo91206 <italoferreiradjk14@gmail.com>
2022-05-12 08:07:18 -0300

    sale implemented

--- src/app/checkout/checkoutModel.js
+++ src/app/checkout/checkoutModel.js
@@ -12,39 +12,61 @@ mercadopago.configure({
 export default {
   async realizarVenda(produtos, codigo_cupom, usuario) {
 
-    // let usuario = req.headers.authorization;
     usuario = jwt.decode(usuario);
-
-    // console.log(req.body);
-    // let produtos = req.body;
-    let valores = [];
+    // console.log("usuario", usuario)
+    let produtos_id = [];
+    let cupom = codigo_cupom || false
+    let desconto = 0;
+    let items_to_push = [];
 
     produtos.forEach((item) => {
-      valores.push(item.produto_id);
+      produtos_id.push(item.produto_id);
     })
 
-    const soma = await connection('produtos')
-      .where((builder) => builder.whereIn('produto_id', valores))
-      .sum('preco')
-      .first();
+    const real_simple_products = await connection('produtos')
+      .where((builder) => builder.whereIn('produto_id', produtos_id))
+      .select('produto_id', 'nome_produto', 'estoque', 'preco')
 
-    const cupom = await connection('cupons')
-      .where('codigo', codigo_cupom)
-      .where('is_enabled', true)
-      .first();
+    // console.log("real_simple_products", real_simple_products)
 
-    const cupom_is_valid = new Date() < cupom.validade ? true : false
-    let desconto = 0;
+    real_simple_products.forEach((p) => {
+      let product_from_request = produtos.filter((pfilter) => {
+        if(pfilter.produto_id == p.produto_id)
+          return pfilter
+      })
+      items_to_push.push({
+        title: p.nome_produto,
+        description: p.descricao,
+        category_id: "fashion",
+        quantity: product_from_request[0].quantidade,
+        currency_id: "BRL",
+        unit_price: p.preco,
+        id: p.produto_id,
+        statement_descriptor: 'WK3OUTLET'
+      })
+    })
 
-    if(cupom_is_valid){
-      if(cupom.is_percent)
-        desconto = Math.abs(soma.sum * (cupom.valor / 100 - 1))
-      else
-        desconto = cupom.valor
+    if(cupom){
+      cupom = await connection('cupons')
+        .where('codigo', codigo_cupom)
+        .where('is_enabled', true)
+        .first();
+      const cupom_is_valid = new Date() < cupom.validade ? true : false
+      if(cupom_is_valid){
+        if(cupom.is_percent)
+          desconto = soma.sum * Math.abs((cupom.valor / 100) - 1)
+        else
+          desconto = cupom.valor
+      }
     }
 
+    var soma = 0
+    items_to_push.forEach((i) => {
+      soma += i.unit_price * i.quantity
+    })
+
     const novaVenda = {
-      total: soma.sum,
+      total: soma,
       desconto: desconto,
       data_venda: new Date(),
       data_envio: null,
@@ -52,22 +74,34 @@ export default {
       prazo_entrega: null,
       rastreio: null,
       usuario_id: usuario.usuario.id,
-      cupom_id: cupom.codigo_cupom || null,
+      cupom_id: cupom ? cupom.codigo_cupom : null,
       endereco_id: null,
       status: 'pendente',
     }
 
-    const venda = await connection('vendas')
+    const venda_id = await connection('vendas')
       .insert(novaVenda, 'venda_id');
 
+    items_to_push.forEach(async (i) => {
+      // console.log({
+      //   quantidade: i.quantity,
+      //   desconto_unitario: 0,
+      //   valor_total: i.quantity * i.unit_price,
+      //   venda_id: venda_id[0],
+      //   produto_id: i.id
+      // })
+      await connection('produtos_vendas')
+        .insert({
+          quantidade: i.quantity,
+          desconto_unitario: 0,
+          valor_total: i.quantity * i.unit_price,
+          venda_id: venda_id[0],
+          produto_id: i.id
+        })
+    })
+
     let preference = {
-      items: [
-        {
-          title: `Venda #${venda}`,
-          unit_price: soma.sum,
-          quantity: 1,
-        }
-      ],
+      items: items_to_push,
       statement_descriptor: 'WK3OUTLET',
       notification_url: 'https://wk3-outlet-backend.herokuapp.com/checkout/notification/',
       back_urls: {
@@ -83,10 +117,10 @@ export default {
     let preference_id = response.body.id;
 
     await connection('vendas')
-      .where('venda_id', venda[0])
+      .where('venda_id', venda_id[0])
       .update('preference_id', preference_id);
 
-    return response.body.init_point;
+    return { url: response.body.init_point, id_venda: venda_id[0] };
   },
 
   async atualizarVenda(data) {
